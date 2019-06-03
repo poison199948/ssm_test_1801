4ab4fba9fe5fd7c34629cab5ff60624b0dd31567
lunai <1306679588@qq.com>
2019-06-03 14:16:42 +0800

    test提交

--- SSM_1801/src/main/java/com/huayu/ssm/test/Test.java
+++ SSM_1801/src/main/java/com/huayu/ssm/test/Test.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-           System.out.println(studentService.queryAll().size());
 //        ClassPathXmlApplicationContext context= new ClassPathXmlApplicationContext("classpath:spring.xml");
 //        StudentService studentService= (StudentService)context.getBean("studentService");
 //        PageHelper.startPage(1,2,true);
